5233ca36988d60e4f25818e96c5a1174df3befd5
Isha Negi <itsishanegi@gmail.com>
2024-09-22 05:12:46 +0530

    Add files via upload

---
+++ printing-star-pattern-using-function.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int star(int totalNumOfStars)
+
+{ 
+  for(int i=0;i<totalNumOfStars;i++)
+  
+{ for(int j=0;j<=i;j++)
+  
+{
+    printf("*");
+  
+}
+  
+printf("\n");
+
+}
+
+}
+
+int main()
+
+{ int totalNumOfStars;
+   
+printf("totalNumOfStars= ") ;
+   
+scanf("%d",&totalNumOfStars);
+   
+star(totalNumOfStars);
+
+}
